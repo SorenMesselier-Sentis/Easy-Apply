81c680fb015ea54766a6fea084eaa9885b99649d
Drouet <quentin.drouet109@gmail.com>
2022-01-03 09:34:19 +0100

    🚧 Add homepage

--- easy-apply/resources/views/home.blade.php
+++ easy-apply/resources/views/home.blade.php
@@ -5,9 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('scss/style.css')}}">
-    <title>ALED</title>
+    <title>Easy-apply</title>
 </head>
 <body>
-    <img src="./img/oui.jpg" alt="">
+    <header>
+        <div>
+            <nav class="navbar">
+                <div>
+                    <a href=""><img src="img/logo.png" alt="logo easy-apply"></a>
+                </div>
+                <div>
+                    <ul class="navbar__main-menu">
+                        <li><a href="">Accueil</a></li>
+                        <li><a href="">Profil</a></li>
+                        <li><a href="">Mes offres</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="" class="button1">Se connecter</a></li>
+                        <li><a href="" class="button2">S'inscrire</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <section class="header-content">
+                <div class="header-content__desc">
+                    <h1>Titre</h1>
+                    <div></div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean luctus mi orci, vitae gravida enim tristique vitae. Sed vehicula enim a urna iaculis convallis. Aliquam facilisis ante magna, ac feugiat tellus convallis at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean luctus mi orci, vitae gravida enim tristique vitae. Sed vehicula enim a urna iaculis convallis. Aliquam facilisis ante magna, ac feugiat</p>
+                </div>
+                <div class="header-content__image">
+                    <img src="img/people.png" alt="people holding phones">
+                </div>
+            </section>
+        </div>
+    </header>
 </body>
 </html>
