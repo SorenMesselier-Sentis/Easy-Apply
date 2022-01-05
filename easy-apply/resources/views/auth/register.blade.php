887ed68496150ca495a122fe3dd44815ed5a4c9c
Adam <adam.ammar@edu.devinci.fr>
2022-01-05 14:44:18 +0100

    :bug: Fix Bug Home + Register

--- easy-apply/resources/views/auth/register.blade.php
+++ easy-apply/resources/views/auth/register.blade.php
@@ -42,7 +42,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Déjà membre test? Se connecter') }}
+                    {{ __('Déjà membre ? Inscrivez vous') }}
                 </a>
 
                 <x-jet-button class="ml-4">
