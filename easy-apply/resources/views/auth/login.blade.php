ef4c716fecd91cf22e91e7bf32bc4af37472c004
Drouet <quentin.drouet109@gmail.com>
2022-01-08 23:58:10 +0100

    💄 Add responsive register and login pages

--- easy-apply/resources/views/auth/login.blade.php
+++ easy-apply/resources/views/auth/login.blade.php
@@ -25,51 +25,53 @@
                     </div>
                 </nav>
         </header>
-        @if (session('status'))
-        <div>
-            {{ session('status') }}
-        </div>
-        @endif
-        <form method="POST" class="card2" action="{{ route('login') }}">
-            @csrf
-            <h2>Connectez-Vous</h2>
+        <section class="login">
+            @if (session('status'))
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" type="email" name="email" :value="old('email')" required autofocus />
+                {{ session('status') }}
             </div>
-
-            <div>
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div>
-                <x-jet-button>
-                    {{ __('Se connecter') }}
-                </x-jet-button>
+            @endif
+            <form method="POST" class="card2" action="{{ route('login') }}">
+                @csrf
+                <h2>Connectez-vous</h2>
                 <div>
-                    <label for="remember_me">
-                    <div class="remember_me">
-                        <x-jet-checkbox id="remember_me" name="remember" />
-                        <span>{{ __('Se souvenir de moi') }}</span>
-                    </div>
-                    </label>
+                    <x-jet-label for="email" value="{{ __('Email') }}" />
+                    <x-jet-input id="email" type="email" name="email" :value="old('email')" required autofocus />
                 </div>
 
-                <div class="butPw">
-                    @if (Route::has('password.request'))
-                    <a href="{{ route('password.request') }}">
-                    {{ __('Mot de passe oublié ?') }}
-                    </a>
-                    @endif
-                    <a href="{{ route('register') }}">Créer un compte</a>
+                <div>
+                    <x-jet-label for="password" value="{{ __('Password') }}" />
+                    <x-jet-input id="password" type="password" name="password" required autocomplete="current-password" />
                 </div>
-            </div>
-            <section class="bottomBar">
-                <div class="orBar"></div>ou<div class="orBar2"></div>
-            </section>
-            <a class="googleConn" href="#">Connection via Google</a>
-        </form>
+
+                <div>
+                    <x-jet-button>
+                        {{ __('Se connecter') }}
+                    </x-jet-button>
+                    <div>
+                        <label for="remember_me">
+                        <div class="remember_me">
+                            <x-jet-checkbox id="remember_me" name="remember" />
+                            <span>{{ __('Se souvenir de moi') }}</span>
+                        </div>
+                        </label>
+                    </div>
+
+                    <div class="butPw">
+                        @if (Route::has('password.request'))
+                        <a href="{{ route('password.request') }}">
+                        {{ __('Mot de passe oublié ?') }}
+                        </a>
+                        @endif
+                        <a href="{{ route('register') }}">Créer un compte</a>
+                    </div>
+                </div>
+                <section class="bottomBar">
+                    <div class="orBar"></div>ou<div class="orBar2"></div>
+                </section>
+                <a class="googleConn" href="#">Connection via Google</a>
+            </form>
+        </section>
     </section>
     @include('partials/footer')
 </body>
